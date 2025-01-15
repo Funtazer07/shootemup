af576b1456838027034706e6520ed399b20034e3
Dvoskins,Andrejs A. <i533293@fontysict.nl>
2025-01-15 16:22:25 +0100

    second try

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
- 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Handle all routes by sending the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}); 
